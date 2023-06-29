735b106992c588204e96e8fc03358421a4c22572
SanketTops <sanket.tops@gmail.com>
2023-06-29 09:26:03 +0530

    Module-1 Condition

---
+++ Module-1/format.py
@@ -0,0 +1,12 @@
+"""id=89
+name='Nirav'
+
+#print("My ID is {} and Name is {}".format(id,name))
+
+print(f"My ID is {id} and Name is {name}")"""
+
+
+a=int(input("Enter A:"))
+b=int(input("Enter B:"))
+
+print(f"A={a}, B={b} and Sum of {a}+{b}={a+b}")
---
+++ Module-1/operators.py
@@ -0,0 +1,15 @@
+a=34
+b=76
+"""print("Add:",a+b)
+print("Sub:",a-b)
+print("Mul:",a*b)
+print("Div:",a/b)"""
+
+
+print("A:",a)
+a+=5 #a=a+5
+print("A:",a)
+a-=5
+print("A:",a)
+a*=5
+print("A:",a)
