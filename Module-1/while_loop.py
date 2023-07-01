ad3ed7b1d495127fe5d9c496e6bab8a21dad0967
SanketTops <sanket.tops@gmail.com>
2023-07-01 09:24:23 +0530

    loops

---
+++ Module-1/while_loop.py
@@ -0,0 +1,21 @@
+"""i=0 #1-initalization
+
+while i<=10: #2-2condition - TRUE
+    #statement
+    print(i)
+    i+=2 #3-inc/dec"""
+
+
+"""i=10
+
+while i>=1:
+    print(i)
+    i-=1"""
+
+
+i=1
+n=5
+while i<=10:
+    #5x1=5
+    print(f"{n} x {i} = {n*i}")
+    i+=1
