7b2f51d29f69d5adb65eb6ff8b34a97d475f4973
SanketTops <sanket.tops@gmail.com>
2023-08-05 09:05:11 +0530

    file+flder

---
+++ Module-3_File's/folder_create.py
@@ -0,0 +1,25 @@
+import os
+
+
+"""os.chdir("Newfolder/Subfolder")
+
+os.mkdir('myfolder')"""
+
+#os.chdir("Newfolder/Subfolder/myfolder")
+#open('myfile.txt','x')
+
+"""os.chdir("Newfolder/Subfolder/myfolder")
+os.remove('myfile.txt')"""
+
+#os.removedirs('Subfolder')
+
+"""os.chdir("Newfolder")
+os.removedirs('Subfolder')"""
+
+
+#os.system('calc')
+#os.system('notepad')
+
+
+#os.startfile('chrome')
+os.startfile('excel')
