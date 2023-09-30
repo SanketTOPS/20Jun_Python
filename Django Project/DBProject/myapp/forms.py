e18958cebe0ad1c735da784fc6e15ce170b21c48
SanketTops <sanket.tops@gmail.com>
2023-09-30 09:27:01 +0530

    Full CRUD

--- Django Project/DBProject/myapp/forms.py
+++ Django Project/DBProject/myapp/forms.py
@@ -6,3 +6,8 @@ class studForm(forms.ModelForm):
         model=studinfo
         fields='__all__'
         #fields=['name','email','dob','mobile','address']
+
+class updateForm(forms.ModelForm):
+    class Meta:
+        model=studinfo
+        fields=['name','email','dob','mobile']
--- Django Project/DBProject/myapp/views.py
+++ Django Project/DBProject/myapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .forms import studForm
+from django.shortcuts import render,redirect
+from .forms import studForm,updateForm
 from .models import *
 
 # Create your views here.
@@ -18,5 +18,20 @@ def showdata(request):
     stdata=studinfo.objects.all()
     return render(request,'showdata.html',{'stdata':stdata})
 
-def updatedata(request):
-    return render(request,'updatedata.html')
+def updatedata(request,id):
+    stid=studinfo.objects.get(id=id)
+    if request.method=='POST':
+        updatedata=updateForm(request.POST,instance=stid)
+        if updatedata.is_valid():
+            updatedata.save()
+            print("Record updated!")
+            return redirect('showdata')
+        else:
+            print(updatedata.errors)
+    return render(request,'updatedata.html',{'stid':stid})
+
+def deletedata(request,id):
+    stid=studinfo.objects.get(id=id)
+    studinfo.delete(stid)
+    return redirect('showdata')
+    
